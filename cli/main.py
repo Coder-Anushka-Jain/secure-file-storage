7ce17d8dad7f2b962f7ee389613724656f828a83
Coder-Anushka-Jain <jainanushka0807@gmail.com>
2025-12-27 18:30:07 +0530

    decription Pipeline

--- cli/main.py
+++ cli/main.py
@@ -1,4 +1,5 @@
-from crypto.file_crypto import encrypt_file
+from crypto.file_crypto import encrypt_file, decrypt_file
 
 if __name__ == "__main__":
     encrypt_file("test.txt", "test.txt.enc")
+    decrypt_file("test.txt.enc", "test_decrypted.txt")
--- crypto/file_crypto.py
+++ crypto/file_crypto.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import json
 import base64
 
-from crypto.aes_utils import generate_aes_key, encrypt_data
-from crypto.hybrid_utils import encrypt_aes_key
+from crypto.aes_utils import generate_aes_key, encrypt_data, decrypt_data
+from crypto.hybrid_utils import encrypt_aes_key, decrypt_aes_key
+
 
 
 def encrypt_file(input_file: str, output_file: str):
@@ -36,3 +37,32 @@ def encrypt_file(input_file: str, output_file: str):
     output_path.write_text(json.dumps(encrypted_bundle))
 
     print(f"[+] File encrypted successfully: {output_file}")
+
+
+
+def decrypt_file(input_file: str, output_file: str):
+    """
+    Decrypts a file encrypted using hybrid encryption.
+    """
+
+    input_path = Path(input_file)
+    output_path = Path(output_file)
+
+    # 1️⃣ Load encrypted bundle
+    encrypted_bundle = json.loads(input_path.read_text())
+
+    encrypted_aes_key = base64.b64decode(encrypted_bundle["encrypted_key"])
+    nonce = base64.b64decode(encrypted_bundle["nonce"])
+    ciphertext = base64.b64decode(encrypted_bundle["ciphertext"])
+
+    # 2️⃣ Decrypt AES key using RSA private key
+    aes_key = decrypt_aes_key(encrypted_aes_key)
+
+    # 3️⃣ Decrypt file using AES-GCM
+    plaintext = decrypt_data(nonce, ciphertext, aes_key)
+
+    # 4️⃣ Write original file back
+    output_path.write_bytes(plaintext)
+
+    print(f"[+] File decrypted successfully: {output_file}")
+
